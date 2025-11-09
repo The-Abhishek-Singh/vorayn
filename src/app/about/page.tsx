f8a39c5d3ddddfb0b93b65dcf48d9ab1c684e6a5
Abhishek Singh <abhi120730@gmail.com>
2025-11-09 23:48:27 +0530

    About Page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import { SmoothScrollHero } from '@/components/about/SmoothScrollHero'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+<SmoothScrollHero />
+
+
+    </div>
+  )
+}
+
+export default page
